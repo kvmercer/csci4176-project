4cd8a008b517a6435a053a3178af603d3b39d249
Kavin Mercer <kv219700@dal.ca>
2022-03-31 19:12:39 -0300

    repo and lank viewmodel

--- app/src/main/java/com/example/eyedroptracker/dao/ReminderDao.kt
+++ app/src/main/java/com/example/eyedroptracker/dao/ReminderDao.kt
@@ -2,11 +2,17 @@ package com.example.eyedroptracker.dao
 
 import androidx.room.*
 import com.example.eyedroptracker.models.Reminder
+import kotlinx.coroutines.flow.Flow
 
 @Dao
 interface ReminderDao {
     @Query("SELECT * FROM reminder")
     suspend fun getAllReminders(): List<Reminder>
+
+    @Query("SELECT * FROM reminder ORDER BY time ASC")
+    fun getRemindersDefault(): Flow<List<Reminder>>
+
+    //TODO: update queries needed
 
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     suspend fun addReminder(reminder: Reminder)
---
+++ app/src/main/java/com/example/eyedroptracker/models/ReminderViewModel.kt
@@ -0,0 +1,4 @@
+package com.example.eyedroptracker.models
+
+class ReminderViewModel {
+}
